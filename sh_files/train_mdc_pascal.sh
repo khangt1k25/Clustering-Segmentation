8b3fde0e2fc5a4717880607da2bb67ba60d50a36
khangt1k25 <khangruni@gmail.com>
2022-03-14 20:21:34 +0700

    update

--- sh_files/train_mdc_pascal.sh
+++ sh_files/train_mdc_pascal.sh
@@ -2,17 +2,15 @@ K_train=20
 K_test=20
 bsize=32
 num_epoch=60
-KM_INIT=64 # need
+KM_INIT=32 # need
 KM_NUM=1 #need
 KM_ITER=20
-SEED=2
+SEED=1
 LR=4e-3
-coeff=0.1
-
 
 mkdir -p results/train/${SEED}
 
-python train_mdc_pascal.py \
+python train_maskcontrast_pascal.py \
 --data_root '/content/drive/MyDrive/UCS_local/PASCAL_VOC' \
 --save_root results/train/${SEED} \
 --pretrain \
@@ -27,4 +25,3 @@ python train_mdc_pascal.py \
 --num_epoch ${num_epoch} \
 --res 224 \
 --augment --jitter --blur --grey --equiv --random_crop --h_flip --v_flip \
---coeff ${coeff}
